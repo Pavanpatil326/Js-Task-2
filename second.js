caa187e0fcdd9005a690c117959184686fef6a9f
Pavanpatil326 <143817491+Pavanpatil326@users.noreply.github.com>
2024-09-05 10:21:28 +0530

    Add second.js

---
+++ second.js
@@ -0,0 +1,29 @@
+ /*Areithmetic Operaters
+ let a = 10;
+  let b = 5;
+ console.log(a + b);
+ console.log(a - b);
+ console.log(a * b);
+ console.log(a / b);
+ console.log(a % b);
+  console.log(a ** b); 
+  */    
+
+ /* unary operators
+let a = 4;
+let b = 5;
+console.log(a++);
+console.log(++a);*/
+
+/*Assignment operators   
+
+let a = 8;
+let b = 7;
+b = a;
+console.log(b); 
+*/
+
+//comarison operators
+
+ let age = 25;
+ console.log(age <= 0); //false 
